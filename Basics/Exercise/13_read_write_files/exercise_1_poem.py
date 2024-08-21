71be4c3368b64d137222ca0ef73c967816eaf85d
ryuk6911 <gauravnarwadkar.gn@gmail.com>
2024-08-21 15:24:13 +0530

    Update exercise_1_poem.py

--- Basics/Exercise/13_read_write_files/exercise_1_poem.py
+++ Basics/Exercise/13_read_write_files/exercise_1_poem.py
@@ -1,21 +1,33 @@
-word_stats={}
+#This version takes punctuation into consideration resulting in "and" becoming the true answer for the maximum occurences in the file Poem.
 
-with open("poem.txt","r") as f:
-    for line in f:
-      words=line.split(' ')
-      for word in words:
-        if word in word_stats:
-          word_stats[word]+=1
-        else:
-          word_stats[word] = 1
+from collections import Counter
+import string
+def words():
+    #Opening a file and reading
+    with open("poem.txt", 'r') as file:
+        text=file.read()
 
-print(word_stats)
 
-word_occurances = list(word_stats.values())
-max_count = max(word_occurances)
-print("Max occurances of any word is:",max_count)
+    #Converting everything to lowercase for the true count
+    text=text.lower()
 
-print("Words with max occurances are: ")
-for word, count in word_stats.items():
-    if count==max_count:
-        print(word)
+    #Removing punctuation so that they don't get processed in the counter
+    text=text.translate(str.maketrans('', '',string.punctuation))
+
+    #Post cleaning splitting it to convert the file into list
+    words=text.split()
+
+    #Calculating the total number of words using len as the file is list with str
+    total_count=len(words)
+
+    #Using Counter to find frequency of all the words
+    word_count=Counter(words)
+
+    max_count=max(word_count.values())
+    for word, count in word_count.items():
+        if count == max_count:
+            print(f"The word with max frquency is/are: '{word}' with maxium occurences of: {count}")
+
+    #print("Total word count is: ",total_count)
+
+words()
